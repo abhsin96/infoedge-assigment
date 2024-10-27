d9a1409e869ac6edb7dd7e99eac376eb26061440
Singh <abhisheksingh93@deloitte.com>
2024-10-28 00:56:48 +0530

    pagination added

--- src/components/template/BlogItem.tsx
+++ src/components/template/BlogItem.tsx
@@ -1,7 +1,7 @@
-import { Grid2 } from "@mui/material";
+import { Grid2, Pagination, Stack } from "@mui/material";
 import BlogCards from "../atom/BlogCards";
 import BlogSkeleton from "../atom/BlogSkeleton";
-import { memo, useMemo } from "react";
+import { memo, useMemo, useState } from "react";
 
 type Props = {
   blogs: {
@@ -13,15 +13,33 @@ type Props = {
 };
 
 const BlogItem = ({ blogs }: Props) => {
+  const [page, setPage] = useState(0);
+  const handlePagenation = (
+    event: React.ChangeEvent<unknown>,
+    page: number
+  ) => {
+    console.log(event.target);
+    setPage(page);
+  };
   if (blogs?.length > 0)
     return (
-      <Grid2 container spacing={2}>
-        {blogs?.map((blog, index: number) => (
-          <Grid2 key={index} size={12}>
-            <BlogCards {...blog} />
-          </Grid2>
-        ))}
-      </Grid2>
+      <Stack direction={"column"} spacing={2}>
+        <Grid2 container spacing={2}>
+          {blogs
+            ?.slice(page * 5, page * 5 + 5)
+            .map((blog, index: number) => (
+              <Grid2 key={index} size={12}>
+                <BlogCards {...blog} />
+              </Grid2>
+            ))}
+        </Grid2>
+        <Pagination 
+          count={blogs?.length/5-1}
+          showFirstButton
+          showLastButton
+          onChange={handlePagenation}
+        />
+      </Stack>
     );
 
   return (
